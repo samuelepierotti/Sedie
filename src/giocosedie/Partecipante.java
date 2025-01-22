47eab2f3d00027193e030106b96cfc2e35839d9d
samuelepierotti <samy.pierotti@gmail.com>
2025-01-22 22:24:52 +0100

    Modifiche alla classe Partecipante.java per salvare in un file di testo la sequenza dei posti occupati

--- src/giocosedie/Partecipante.java
+++ src/giocosedie/Partecipante.java
@@ -1,9 +1,17 @@
+package giocosedie;
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+
 /**
  * Classe Partecipante rappresenta un giocatore
  */
 class Partecipante extends Thread {
     /** Array dei posti disponibili nel gioco */
     private Posto sedie[];
+    
+    private static final String FILE_NAME = "Risultato.txt";
 
     /**
      * Costruttore della classe Partecipante.
@@ -22,9 +30,15 @@ class Partecipante extends Thread {
             // Pausa iniziale casuale per far partire i giocatori in momenti diversi
             sleep((int) (Math.random() * 1000));
 
-            // Partecipante cerca di occupare un posto
+            // Il giocatore cerca di occupare un posto
             for (int i = 0; i < sedie.length; i++) {
                 if (sedie[i].occupa()) {
+                    // Scrive il risultato nel file
+                    synchronized (Partecipante.class) { // Sincronizzazione per evitare conflitti tra thread
+                        try (BufferedWriter writer = new BufferedWriter(new FileWriter(FILE_NAME, true))) {
+                            writer.write("Posto " + i + " occupato dal thread " + this.getName() + "\n");
+                        }
+                    }
                     System.out.println("Sono il Thread " + this.getName()
                             + ". Sono riuscito a sedermi sul posto " + i);
                     return;
@@ -36,6 +50,8 @@ class Partecipante extends Thread {
 
         } catch (InterruptedException e) {
             throw new RuntimeException(e);
+        } catch (IOException e) {
+            System.out.println("Errore durante la scrittura nel file");
         }
     }
 }
