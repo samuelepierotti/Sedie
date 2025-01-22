fb2c41029680adb1d66151c77ac8f1eedd46e336
samuelepierotti <samy.pierotti@gmail.com>
2025-01-22 18:12:32 +0100

    Commento classe Posto.java

--- src/giocosedie/Posto.java
+++ src/giocosedie/Posto.java
@@ -1,24 +1,37 @@
-package giocosedie;
+/**
+ * Classe Posto rappresenta una sedia nel gioco.
+ */
+class Posto {
+    /** Flag che indica se il posto è occupato */
+    private boolean occupato;
 
-class Posto
+    /**
+     * Costruttore della classe Posto. Inizializza il posto come libero.
+     */
+    public Posto() {
+        occupato = false;
+    }
 
-{
-	private boolean occupato;
+    /**
+     * Metodo sincronizzato per controllare se il posto è libero.
+     * 
+     * restituisce true se il posto è libero, altrimenti false
+     */
+    public synchronized boolean libero() {
+        return (!occupato);
+    }
 
-	public Posto()
-
-	{
-		occupato = false;
-	}
-
-	public synchronized boolean libero() {
-		return (!occupato);
-	}
-
-	public synchronized boolean occupa() {
-		if (occupato)
-			return false;
-		else
-			return occupato = true;
-	}
+    /**
+     * Metodo sincronizzato per occupare il posto se è libero.
+     * 
+     * restituisce true se l'occupazione ha avuto successo, altrimenti false
+     */
+    public synchronized boolean occupa() {
+        if (occupato) {
+            return false;
+        } else {
+            occupato = true;
+            return true;
+        }
+    }
 }
